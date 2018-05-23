4eba7cfd3138492a7a3251b5c229fa272460dd0a
Graham Nott <gnott@starglobal.ca>
2018-05-22 19:37:58 -0700

    Rename the app() function back to something else, it may be a name conflict.

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def upload_form():
     return form_content
 
 @app.route('/', methods=['GET', 'POST'])
-def app():
+def main():
     # handle the uploaded file
     url_for('static', filename='prism.js')
     url_for('static', filename='prism.css')
